e7eea05417e59df032406c7cb15fa8e03ed842bd
aprilespiritu <april_espiritu@live.com>
2024-06-24 15:06:46 +0800

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,10 @@ import { Statistics } from './Statistics/Statistics';
 import data from '../data/data.json';
 
 import { FriendList } from './FriendList/FriendList';
-import friends from '../data/friends.json'
+import friends from '../data/friends.json';
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -20,6 +23,7 @@ export const App = () => {
 
       <Statistics title="Upload Stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
