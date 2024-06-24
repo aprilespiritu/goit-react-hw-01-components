37ab0450b46e1276a3769b0492669bb559414b75
aprilespiritu <april_espiritu@live.com>
2024-06-24 11:20:20 +0800

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
+import { Profile } from './Profile/Profile';
+import user from '../data/user.json';
+
+
 import { FriendList } from './FriendList/FriendList';
 import friends from '../data/friends.json'
 
 export const App = () => {
   return (
     <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <FriendList friends={friends} />
     </>
   );
